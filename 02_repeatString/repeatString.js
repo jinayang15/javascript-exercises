35963d676a6d1de3272fb8e91d42519b450e2f19
Jina Yang <jinayang15@gmail.com>
2024-05-10 21:14:09 -0400

    Finish Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    try {
+        return text.repeat(num);
+    } catch {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
