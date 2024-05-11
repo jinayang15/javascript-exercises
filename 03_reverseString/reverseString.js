fa12cd46375cfbebac665e54e9bc564a591a1dd0
Jina Yang <jinayang15@gmail.com>
2024-05-10 21:20:33 -0400

    Finish Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reversed = "";
+    for (let i = text.length-1; i >= 0; i--) {
+        reversed += text[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
