527fec15f24a5a5a44f48e6f8ab94b56dd5af0b5
Jina Yang <jinayang15@gmail.com>
2024-05-13 08:56:51 -0400

    Finish Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < args.length; j++) {
+            if (arr[i] === args[j]) break;
+            if (j == args.length-1) {
+                newArr.push(arr[i]);
+            }
+        }
+    }
+    return newArr;
+    
 };
 
 // Do not edit below this line
