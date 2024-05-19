163deb6a2c70e950e8d60e6ccbae0db01e5849e0
Jina Yang <jinayang15@gmail.com>
2024-05-19 12:26:26 -0400

    Finish Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.match(/[\w]/).join("");
+    let left = 0;
+    let right = str.length-1;
+    while (left <= right) {
+        if (str.charAt(left) != str.charAt(right)) return false;
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
