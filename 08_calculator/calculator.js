8ee2c3fcd2861b4c6fd161398feeb682dc2000da
Jina Yang <jinayang15@gmail.com>
2024-05-19 12:08:00 -0400

    Finish Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	let product = 1;
+  for (let i = 0; i < exp; i++) {
+    product *= base;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
