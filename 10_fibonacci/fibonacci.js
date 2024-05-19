efaa4a8cb17803a49802bb800e106ac8f7d3e4dd
Jina Yang <jinayang15@gmail.com>
2024-05-19 13:34:01 -0400

    Finish Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    if (num <= 2) return 1;
+    let prev = 1;
+    let cur = 1;
+    for (let i = 3; i <= num; i++) {
+        const next = prev + cur;
+        prev = cur;
+        cur = next;
+    }
+    return cur;
 
 };
 
